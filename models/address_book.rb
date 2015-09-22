8f1021867bcd95a0a7851d8469cfb7240d96a255
Daniel Peluso <thedanielpeluso@gmail.com>
2015-09-22 09:13:26 -0700

    Pell exploring git stuff

---
+++ models/address_book.rb
@@ -0,0 +1,39 @@
+require_relative 'entry'
+require "csv"
+
+class AddressBook
+  attr_accessor :entries
+
+  def initialize
+    @entries = []
+  end
+  def add_entry(name, phone_number, email)
+    index = 0
+    @entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1 # index = index + 1
+    end
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+  def remove_entry(name, phone_number, email)
+    index = 0
+    @entries.each do |entry|
+      if name == entry.name
+        break
+      end
+      index += 1
+    end
+    @entries.delete_at(index)
+  end
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+ end
+end
